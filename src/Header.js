eac3b31a960959328eaa3a746647b96b95b315cf
yass18677 <lenayacine2007@gmail.com>
2026-05-02 15:28:36 +0000

    Setup initial SenTransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-titre">SenTransport</h1>
+      <p className="header-soustitre">
+        Votre guide du transport en commun a Dakar
+      </p>
+    </header>
+  );
+}
+
+export default Header;
